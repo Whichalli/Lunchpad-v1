5f9bdde09d35510890145a710af0b853a5b52a25
Whichalli <137446284+Whichalli@users.noreply.github.com>
2023-10-15 11:20:42 +0400

    Update Payment.jsx

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,8 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useFlutterwave } from 'flutterwave-react-v3';
-import PaymentButton from './PaymentButton';
-import Button from '@material-ui/core/Button';
-import GenerateQRCode from './GenerateQRCode';
 
 const Payment = () => {
   
